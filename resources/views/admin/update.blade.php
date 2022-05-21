4258b97f5fdbb42807f3a2b6d5e8390e6bfbc0e9
justinas-coder <justas.ankenas@gmail.com>
2022-05-21 15:00:40 +0300

    Edit page updated

--- resources/views/admin/update.blade.php
+++ resources/views/admin/update.blade.php
@@ -10,29 +10,27 @@
 
     <div class="col-sm-4 mb-4">
 
-        <form action="{{route('asset.update', $asset->id)}}" method="post" enctype="multipart/form-data">
+        <form action="{{route('asset.update', $asset->id)}}" method="post">
             @method('PATCH')
             @csrf
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="title">Title</label>
                 <input type="text" name="title" class="form-control" id="title" aria-describeby=""
                        placeholder="Enter title"
                        value="{{$asset->title}}">
             </div>
 
-
-
-            <div class="form-group mb-3">
-                <label for="crypto_currency">Crypto_currency</label>
-
-                <select class="custom-select" id="crypto_currency" name="crypto_currency" placeholder="Enter Currency"
-                        value="{{$asset->crypto_currency}}">
-                    <option selected>Choose...</option>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="crypto_currency">Crypto_currency</label>
+                </div>
+                <select class="custom-select" id="currency" name="currency">
                     @foreach ($currencies as $currency)
                         <option value="{{$currency->name}}">{{$currency->name}}</option>
-                    @endforeach
-                </select>
+                    @endforeach                </select>
             </div>
+
+
             <div class="form-group mb-3">
                 <label for="paid_value">Paid Value</label>
                 <input type="number" name="paid_value" class="form-control" id="paid_value" aria-describeby=""
@@ -44,7 +42,7 @@
                        placeholder="" value="{{$asset->quantity}}">
             </div>
 
-            <div class="input-group mb-3">
+            <div class="input-group mb-5">
                 <div class="input-group-prepend">
                     <label class="input-group-text" for="currency">Options</label>
                 </div>
